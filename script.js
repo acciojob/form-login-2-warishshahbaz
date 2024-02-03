ce1574379df4e1265bd61fb13f3a6bd97860ec09
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-03 23:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 //your JS code here. If required.
-let btn = document.getElementById("MyForm");
-function handleSubmit(){
-	
-alert("fdsfs")
-	let first = document.getElementById("firstname").value
-	let last = document.getElementById("lastname").value
-	let phone = document.getElementById("phone").value
-	let email = document.getElementById("email").value
-	alert(`
+function handleSubmit() {
+  let first = document.getElementById("firstname").value;
+  let last = document.getElementById("lastname").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  alert(`
 	First Name : ${first}
 	Last Name : ${last}
 	Phone Number : ${phone}
 	Email ID : ${email}
-	`)
+	`);
 }
-// btn.addEventListner("click",(e)=>{
-// 	e.preventDefault();
-// 	let first = document.getElementById("firstname").value
-// 	let last = document.getElementById("lastname").value
-// 	let phone = document.getElementById("phone").value
-// 	let email = document.getElementById("email").value
-// 	alert(`
-// 	First Name : ${first}
-// 	Last Name : ${last}
-// 	Phone Number : ${phone}
-// 	Email ID : ${email}
-// 	`)
-// })
-})
+
+
