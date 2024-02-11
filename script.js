fc77e46ae84e4efb4c896a522754ee05b285bfa9
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-11 16:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-//your JS code here. If required.
-function handleSubmit() {
-  let first = document.getElementById("firstname").value;
-  let last = document.getElementById("lastname").value;
-  let phone = document.getElementById("phone").value;
-  let email = document.getElementById("email").value;
-  alert(`
-	First Name : ${first}
-	Last Name : ${last}
-	Phone Number : ${phone}
-	Email ID : ${email}
-	`);
-}
+ function submitForm() {
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var email = document.getElementById("email").value;
 
-
+            var alertMessage = "First Name: " + firstName + " Last Name: " + lastName + " Phone Number: " + phoneNumber + " Email ID: " + email;
+            alert(alertMessage);
+            return false; // Prevent form submission
+        }
